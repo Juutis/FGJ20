1aa09f1afdfc20b02b52099100d787fa93dbf91b
Juutis <jutkula@gmail.com>
2020-02-01 16:27:42 +0200

    Made mothership modules breakable

---
+++ Game/Untitled FGJ 2020 Project/Assets/MotherShip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherShip : MonoBehaviour
+{
+    List<ShipPart> shipParts = new List<ShipPart>(),
+        availableParts = new List<ShipPart>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipParts.AddRange(GetComponentsInChildren<ShipPart>());
+        availableParts.AddRange(shipParts);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LaunchRandomPart();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            foreach (var part in shipParts)
+            {
+                part.Repair();
+            }
+        }
+    }
+
+    public void LaunchRandomPart()
+    {
+        if (availableParts.Count <= 0)
+        {
+            return;
+        }
+        var part = getRandomPart();
+        var force = new Vector2(Random.Range(-3000f, 3000f), Random.Range(-3000f, 3000f));
+        part.Launch(force);
+        availableParts.Remove(part);
+    }
+
+    private ShipPart getRandomPart()
+    {
+        int idx = Random.Range(0, availableParts.Count);
+        return availableParts[idx];
+    }
+}
---
+++ Game/Untitled FGJ 2020 Project/Assets/ShipPart.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPart : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Collider2D coll;
+
+    float collisionDisabledUntil;
+
+    Vector3 startPos;
+    Quaternion startRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        startPos = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!coll.enabled && collisionDisabledUntil < Time.time)
+        {
+            coll.enabled = true;
+        }
+    }
+
+    public void Launch(Vector2 force)
+    {
+        rb.AddForce(force);
+        collisionDisabledUntil = Time.time + 3.0f;
+        coll.enabled = false;
+        rb.simulated = true;
+    }
+
+    public void Repair()
+    {
+        transform.position = startPos;
+        transform.rotation = startRotation;
+        rb.simulated = false;
+    }
+}
