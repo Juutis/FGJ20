139d1bbfe96261854bb194a8e12a4118f4a61583
Matias Pitkänen <M2tias@GMail.com>
2020-02-01 16:21:38 +0200

    :gun: Cannon changes

---
+++ Game/Untitled FGJ 2020 Project/Assets/Scripts/DestroyableCannon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableCannon : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHP = 5;
+    private int currentHP = 5;
+    [SerializeField]
+    private GameObject destroyedParts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+            destroyedParts.transform.parent = null;
+            destroyedParts.SetActive(true);
+            Vector3 center = transform.position;
+            foreach(Transform child in destroyedParts.transform)
+            {
+                Vector3 direction = child.position - center;
+                child.GetComponent<Rigidbody2D>().AddForce(direction.normalized*100);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "PlayerProjectile")
+        {
+            currentHP--;
+        }
+    }
+}
